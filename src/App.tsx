a3e9f229e3079666ca3b1d4dd567ffba55ee1a74
José Felipe Alexandre Martins <felipealexandre178@gmail.com>
2024-07-23 16:12:16 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import CharacterDetails from "./pages/CharacterDetails";
 import Locations from "./pages/locations";
 import LocationDetails from "./pages/LocationDetails";
 import Episodes from "./pages/episodes";
+import EpisodeDetails from "./pages/EpisodeDetails/EpisodeDetails";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/locations" element={<Locations />} />
         <Route path="/location/:id" element={<LocationDetails />} />
         <Route path="/episodes" element={<Episodes />} />
+        <Route path="/episode/:id" element={<EpisodeDetails />} />
       </Routes>
     </BrowserRouter>
   );
