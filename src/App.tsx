1e8b9d81d7c7d1c3162b30b792f513fd453e2bb1
José Felipe Alexandre Martins <felipealexandre178@gmail.com>
2024-07-24 12:21:36 -0300

    att final.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Characters from "./pages/characters";
